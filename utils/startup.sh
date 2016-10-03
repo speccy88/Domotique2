381fd91b1488d73ace3dfabdaf451e787b53cb52
Fred <fred_blais5@hotmail.com>
2016-10-03 13:27:18 -0400

    app need at least devices database created

--- utils/startup.sh
+++ utils/startup.sh
@@ -9,8 +9,12 @@ sudo apt-get -y install git vim htop couchdb python3 python3-pip
 #Required Python 3 libraries
 sudo pip3 install tornado couchdb astral
 
-#Allow CouchDB access from any IP address
+#Allow CouchDB access from any IP address and restart the process
 sudo sed -i "/bind_address = 127.0.0.1/c\bind_address = 0.0.0.0" /etc/couchdb/default.ini
+sudo systemctl restart couchdb
+
+#Create devices database
+curl -X PUT http://127.0.0.1:5984/devices
 
 #Customize VIM
 cat <<EOT > ~/.vimrc
